4b77dd5d3ea39df850af85287f5c73327e218c77
Stan <35483548+StanBTeh@users.noreply.github.com>
2020-05-17 11:24:35 +0800

    Updates

---
+++ precache-manifest.dd0f3d8cb43042708cd488436787777a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a7397e20cbfe9e7798601e80d157eb",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8011684adad08aee3759",
+    "url": "/weather/static/css/main.b4037c76.chunk.css"
+  },
+  {
+    "revision": "a470a79098bb76a01380",
+    "url": "/weather/static/js/2.3653a6eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.3653a6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8011684adad08aee3759",
+    "url": "/weather/static/js/main.dba28526.chunk.js"
+  },
+  {
+    "revision": "93f41cbaddd6189c9424",
+    "url": "/weather/static/js/runtime-main.b405a412.js"
+  }
+]);
