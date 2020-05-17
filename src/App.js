4bf1ca0ab6bd17019c382ad241ce98d2ce5d7ace
Stan <35483548+StanBTeh@users.noreply.github.com>
2020-05-17 11:10:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import WeatherCard from "./components/WeatherCard/component";
 function App() {
   return (
     <div className="App">
-      <WeatherCard temp="19" city="Stan" />
+      <WeatherCard temp="21" city="Stan" />
       <WeatherCard temp="25" city="Nadine" />
       <WeatherCard temp="40" city="Ysabelle" />
     </div>
